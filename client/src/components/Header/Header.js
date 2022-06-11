2bc2bae04b4f481538dbd8ad44698bae778d6d9b
Mabilog <g.angelo.m@gmail.com>
2022-06-10 20:56:15 -0400

    Header.js created and exported

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import Search from "./Search";
+import Logo from "./Logo";
+import Auth from "../../Auth";
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Container>
+        <Left>
+          <Logo />
+        </Left>
+        <Right>
+          <Search />
+          <Auth />
+        </Right>
+      </Container>
+    </HeaderWrapper>
+  );
+};
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 75px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 80%;
+  max-width: 1800px;
+  height: ;
+`;
+
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+`;
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 50%;
+  gap: 20px;
+`;
+
+export default Header;
